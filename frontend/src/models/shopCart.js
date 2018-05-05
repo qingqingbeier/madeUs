649a39719b05cf2be8ff9ae6e93fa58b9fd41332
wuyunjiang <335208671@qq.com>
2018-05-06 01:42:15 +0800

    更新菜单栏样式，解决刷新页面购物车框清零问题

--- frontend/src/models/shopCart.js
+++ frontend/src/models/shopCart.js
@@ -9,10 +9,7 @@ export default {
 
   subscriptions: {
     setup({ dispatch, history }) {  // eslint-disable-line
-      dispatch({
-        type:'setInitData',
-        cartData:window.localStorage.cartData?JSON.parse(window.localStorage.cartData):[]
-      })
+      dispatch({type:'setInitData'})
     },
   },
 
@@ -23,6 +20,11 @@ export default {
   },
 
   reducers: {
+    setInitData(state, action) {
+      let whoCart = window.localStorage.userName||"noLogin"
+      const cartData = window.localStorage[whoCart+"Cart"]||"[]"
+      return { ...state, cartData:JSON.parse(cartData) };
+    },
     addToCart(state, action) {
       const cartData = state.cartData.concat(action.payload.goodsData)
       let whoCart = window.localStorage.userName||"noLogin"
